83f836e29e12e106d19a3dd9a9e5f8abb8237994
1111AZZZZAZZZZ <2827543855@qq.com>
2026-03-18 09:04:23 +0800

    start

---
+++ src/main/java/com/nongcetong/nongcetongbackend/NongcetongApplication.java
@@ -0,0 +1,13 @@
+package com.nongcetong.nongcetongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongcetongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NongcetongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nongcetong/nongcetongbackend/NongcetongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nongcetong.nongcetongbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NongcetongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
